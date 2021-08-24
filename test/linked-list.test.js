0553c10db24567a96215a9452222fef2f43e47ba
Chris McAnally <chris@adadevelopersacademy.org>
2021-08-24 11:55:19 -0700

    rearrange tests to match python version

--- test/linked-list.test.js
+++ test/linked-list.test.js
@@ -104,87 +104,7 @@ describe("LinkedList", () => {
         });
     });
 
-    describe("max and min values", () => {
-        it("returns null if the list is empty", () => {
-            expect(list.findMax()).toBeNull();
-            expect(list.findMin()).toBeNull();
-        });
-
-        it("can retrieve the max and min values in the list", () => {
-            let count = 0;
-            while (count < 5) {
-                list.addFirst(count);
-                expect(list.findMax()).toEqual(count);
-                expect(list.findMin()).toEqual(0);
-                count += 1
-            }
-
-            list.addFirst(100);
-            list.addFirst(-12);
-
-            expect(list.findMax()).toEqual(100);
-            expect(list.findMin()).toEqual(-12);
-        });
-    });
-
-    describe("delete", () => {
-        it("deleting from empty list is a no-op", () => {
-            expect(list.length()).toEqual(0);
-            list.delete(4);
-            expect(list.length()).toEqual(0);
-        });
-
-        it("can delete valid values from a list", () => {
-            list.addFirst(9);
-            list.addFirst(10);
-            list.addFirst(4);
-            list.addFirst(3);
-            list.addFirst(2);
-
-            // delete first node (requires updating head)
-            list.delete(2)
-            expect(list.getAtIndex(0)).toEqual(3)
-            expect(list.length()).toEqual(4)
-            expect(list.getAtIndex(list.length() - 1)).toEqual(9)
-            expect(list.findMax()).toEqual(10);
-            expect(list.findMin()).toEqual(3);
-
-
-            list.delete(10)
-            expect(list.getAtIndex(0)).toEqual(3)
-            expect(list.length()).toEqual(3)
-            expect(list.getAtIndex(list.length() - 1)).toEqual(9)
-            expect(list.findMax()).toEqual(9);
-            expect(list.findMin()).toEqual(3);
-
-            list.delete(4)
-            expect(list.getAtIndex(0)).toEqual(3)
-            expect(list.length()).toEqual(2)
-            expect(list.getAtIndex(list.length() - 1)).toEqual(9)
-            expect(list.findMax()).toEqual(9);
-            expect(list.findMin()).toEqual(3);
-        });
-    });
-
-    describe("reverse", () => {
-        it("can reverse the linked list", () => {
-            // Arrange
-            list.addFirst(4);
-            list.addFirst(3);
-            list.addFirst(2);
-            list.addFirst(1);
-
-            list.reverse();
-
-            // Assert
-            expect(list.getAtIndex(0)).toEqual(4);
-            expect(list.getAtIndex(1)).toEqual(3);
-            expect(list.getAtIndex(2)).toEqual(2);
-            expect(list.getAtIndex(3)).toEqual(1);
-        });
-    })
-
-    describe.skip("Optional: addLast and getLast", () => {
+    describe.skip("addLast and getLast", () => {
         it("will put new items to the rear of the list", () => {
             list.addLast(2);
             expect(list.length()).toEqual(1);
@@ -210,76 +130,78 @@ describe("LinkedList", () => {
         })
     });
 
-    describe.skip("Optional: findNthFromEend", () => {
-        it("will return null if the index is outside the bounds of the list", () => {
-            // Assert
-            expect(list.findNthFromEnd(3)).toBeNull();
+
+    describe("findMax", () => {
+        it("returns null if the list is empty", () => {
+            expect(list.findMax()).toBeNull();
         });
 
-        it("can retrieve an item at index n from the end in the list", () => {
-            list.addFirst(1);
-            list.addFirst(2);
-            list.addFirst(3);
-            list.addFirst(4);
+        it("can retrieve the max value in the list", () => {
+            let count = 0;
+            while (count < 5) {
+                list.addFirst(count);
+                expect(list.findMax()).toEqual(count);
+                count += 1
+            }
 
-            // Assert
-            expect(list.findNthFromEnd(1)).toEqual(1);
-            expect(list.findNthFromEnd(2)).toEqual(2);
-            expect(list.findNthFromEnd(3)).toEqual(3);
-            expect(list.findNthFromEnd(4)).toEqual(4);
+            list.addFirst(100);
+            list.addFirst(-12);
+
+            expect(list.findMax()).toEqual(100);
         });
     });
 
-    describe.skip("Optional: insertAscending", () => {
-        it("inserts an element in the middle of the list", () => {
-            // Arrange
-            list.addFirst(5);
-            list.addFirst(4);
-            list.addFirst(2);
-            list.addFirst(1);
-
-            // Act 
-            list.insertAscending(3);
-
-            // Assert
-            expect(list.getAtIndex(2)).toEqual(3);
+    describe("delete", () => {
+        it("deleting from empty list is a no-op", () => {
+            expect(list.length()).toEqual(0);
+            list.delete(4);
+            expect(list.length()).toEqual(0);
         });
 
-        it("inserts an element at the beginning of the list", () => {
-            // Arrange
-            list.addFirst(5);
+        it("can delete valid values from a list", () => {
+            list.addFirst(9);
+            list.addFirst(10);
             list.addFirst(4);
             list.addFirst(3);
             list.addFirst(2);
 
-            // Act 
-            list.insertAscending(1);
+            // delete first node (requires updating head)
+            list.delete(2)
+            expect(list.getAtIndex(0)).toEqual(3)
+            expect(list.length()).toEqual(4)
+            expect(list.getAtIndex(list.length() - 1)).toEqual(9)
+            expect(list.findMax()).toEqual(10);
 
-            // Assert
-            expect(list.getAtIndex(0)).toEqual(1);
+
+            list.delete(10)
+            expect(list.getAtIndex(0)).toEqual(3)
+            expect(list.length()).toEqual(3)
+            expect(list.getAtIndex(list.length() - 1)).toEqual(9)
+            expect(list.findMax()).toEqual(9);
+
+            list.delete(4)
+            expect(list.getAtIndex(0)).toEqual(3)
+            expect(list.length()).toEqual(2)
+            expect(list.getAtIndex(list.length() - 1)).toEqual(9)
+            expect(list.findMax()).toEqual(9);
         });
+    });
 
-        it("inserts an element at the end of the list", () => {
+    describe("reverse", () => {
+        it("can reverse the linked list", () => {
             // Arrange
             list.addFirst(4);
             list.addFirst(3);
             list.addFirst(2);
             list.addFirst(1);
 
-            // Act 
-            list.insertAscending(5);
+            list.reverse();
 
             // Assert
-            expect(list.getAtIndex(4)).toEqual(5);
-        });
-
-        it("inserts an element in an empty list", () => {
-            // Act 
-            list.insertAscending(5);
-
-            // Assert
-            expect(list.getAtIndex(0)).toEqual(5);
-            expect(list.length()).toEqual(1);
+            expect(list.getAtIndex(0)).toEqual(4);
+            expect(list.getAtIndex(1)).toEqual(3);
+            expect(list.getAtIndex(2)).toEqual(2);
+            expect(list.getAtIndex(3)).toEqual(1);
         });
     });
 
@@ -304,6 +226,26 @@ describe("LinkedList", () => {
             expect(list.findMiddleValue()).toEqual(3);
         });
     });
+
+    describe.skip("Optional: findNthFromEend", () => {
+        it("will return null if the index is outside the bounds of the list", () => {
+            // Assert
+            expect(list.findNthFromEnd(3)).toBeNull();
+        });
+
+        it("can retrieve an item at index n from the end in the list", () => {
+            list.addFirst(1);
+            list.addFirst(2);
+            list.addFirst(3);
+            list.addFirst(4);
+
+            // Assert
+            expect(list.findNthFromEnd(1)).toEqual(1);
+            expect(list.findNthFromEnd(2)).toEqual(2);
+            expect(list.findNthFromEnd(3)).toEqual(3);
+            expect(list.findNthFromEnd(4)).toEqual(4);
+        });
+    });    
 
     describe.skip("Optional: hasCycle", () => {
         it("will return true when a cycle is present", () => {
